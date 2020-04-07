985e63f8ea69eb3c9b4826042b0079f854df1202
Ubuntu <ubuntu@ip-172-31-31-172.us-west-2.compute.internal>
2020-04-07 20:11:37 +0000

    got the fetch working for Puppy

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,57 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
-import PropTypes from "prop-types"
+
 class App extends React.Component {
+  constructor(){
+    super()
+    
+    this.accessCORS = 'https://cors-anywhere.herokuapp.com/'
+    
+    this.state = {
+      recipePuppy: {},
+      error: null
+    }
+    
+  }
+  
+  
+  componentDidMount(){
+    this.getDUDES()
+  }
+  
+  
+  
+  // Test ONIONS
+  getDUDES = () => {
+    
+    //https://medium.com/@dtkatz/3-ways-to-fix-the-cors-error-and-how-access-control-allow-origin-works-d97d55946d9
+    //http://www.recipepuppy.com/about/api/
+    fetch(`${this.accessCORS}http://www.recipepuppy.com/api/`, 
+      { 
+        headers: { 'Content-Type': 'application/json' }
+      })
+    .then((response)=>{
+      //console.table(response)
+        if(response.status === 200)
+        { return(response.json()) }
+    })
+    .then((resultsArray)=>{
+      console.log(resultsArray)
+        this.setState({ recipePuppy: resultsArray }) 
+    })
+    .catch((error) => this.setState({error}))
+  }
+  
+  /* = * = * = * = * = * = * = * = * = * = * = * = * = * = * = */
+  
+  
+  
+  
   render () {
+    
+    
+    console.table(this.state.recipePuppy.results)
      const {
       logged_in,
       sign_in_route,
