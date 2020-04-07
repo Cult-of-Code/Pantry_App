05c696bd87af0023cb24ca927007e8ce6a2327a1
Ubuntu <ubuntu@ip-172-31-31-83.us-west-2.compute.internal>
2020-04-07 22:37:24 +0000

    added CRUD functions to controllers

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,39 @@
 class IngredientsController < ApplicationController
+    def index
+        ingredients = Ingredient.all
+        render json: ingredients
+    end
+    
+    def update 
+        ingredient = Ingredient.find(params[:id])
+        ingredient.update_attributes(ingredient_params)
+        render json: ingredient
+    end
+    
+    def destroy
+        Ingredient.destroy(params[:id])
+    end
+    
+    def create
+         ingredient = Ingredient.create(ingredient_params)
+        if ingredient.valid?
+        render json: ingredient
+        else
+        render json: ingredient.errors, status: :unprocessable_entity
+        end
+    end
+    
+    
+    def show
+        ingredient = Ingredient.find(params[:id])
+        render json: ingredient
+    end
+    
+    private
+    
+        def ingredient_params
+            params.require(:ingredient).permit(:name, :amount, :unit)
+        end
+
+    
 end
--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -1,2 +1,39 @@
 class UserRecipesController < ApplicationController
+    def index
+        userRecipes = UserRecipe.all
+        render json: userRecipes
+    end
+    
+    def update 
+        userRecipe = UserRecipe.find(params[:id])
+        userRecipe.update_attributes(userRecipe_params)
+        render json: userRecipe
+    end
+    
+    def destroy
+        UserRecipe.destroy(params[:id])
+    end
+    
+    def create
+         userRecipe = UserRecipe.create(userRecipe_params)
+        if userRecipe.valid?
+        render json: userRecipe
+        else
+        render json: userRecipe.errors, status: :unprocessable_entity
+        end
+    end
+    
+    
+    def show
+        userRecipe = UserRecipe.find(params[:id])
+        render json: userRecipe
+    end
+    
+    private
+    
+        def userRecipe_params
+            params.require(:userRecipe).permit(:name, :description, :est_time)
+        end
+
+    
 end
