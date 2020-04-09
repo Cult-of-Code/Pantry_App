71e7cb514c6c463f569099bcb98f391ac1ed3ba5
Ubuntu <ubuntu@ip-172-31-25-177.us-east-2.compute.internal>
2020-04-09 20:20:55 +0000

    SetUp Mock data

---
+++ app/javascript/components/mockData.js
@@ -0,0 +1,57 @@
+// This is were the mock data for the frontend is stored
+
+const pantry_item = [
+    {
+        id: 1,
+        name: 'Apple',
+        quantity: 3.0,
+        units: "Grams",
+        storage_bin: 'fridge',
+        when_bought: '2019-11-21 6:32:00',
+        exp_date: '',
+        min_item: 2,
+        max_item: 5,
+    },{
+        id: 2,
+        name: 'Bannana',
+        quantity: 1.5,
+        units: "Pounds",
+        storage_bin: 'Counter',
+        when_bought: '2020-01-5 12:52:20',
+        exp_date: '',
+        min_item: 0,
+        max_item: 2,
+    },{
+        id: 3,
+        name: 'Spam',
+        quantity: 40.0,
+        units: "Boxes",
+        storage_bin: 'Pantry',
+        when_bought: '2010-07-01 02:33:34',
+        exp_date: '2019-10-31 00:13:39',
+        min_item: 5,
+        max_item: 20,
+    },{
+        id: 4,
+        name: 'Gummy',
+        quantity: 1.0,
+        units: "Bags",
+        storage_bin: 'fridge',
+        when_bought: '',
+        exp_date: '',
+        min_item: 4,
+        max_item: 7,
+    },{
+        id: 5,
+        name: 'Sugar',
+        quantity: .25,
+        units: "Cups",
+        storage_bin: 'Pantry',
+        when_bought: '2019-02-29 11:59:59',
+        exp_date: '',
+        min_item: 1,
+        max_item: 5,
+    },
+]
+
+export default pantry_item
