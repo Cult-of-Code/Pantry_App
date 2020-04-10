71c96dd9906dd736ce191a81aec599a3c64e0dcd
Ubuntu <ubuntu@ip-172-31-25-177.us-east-2.compute.internal>
2020-04-10 17:30:18 +0000

    Items are now stored inside their storage bins

--- app/javascript/components/pages/ViewItemsInPantry.js
+++ app/javascript/components/pages/ViewItemsInPantry.js
@@ -8,7 +8,7 @@ class ViewItemsInPantry extends Component {
     constructor(props){
         super(props)
         this.state = {
-          items: pantry_item
+          items: pantry_item,
         }
     }
     
@@ -16,28 +16,46 @@ class ViewItemsInPantry extends Component {
     
     
     render(){
-        
+        {var storage_bin = []}
+        {var single_bin = ''}
         return (
-            <Col style={{padding: '10px'}}>
-                <Card body>
-                    <Row>
-                        { this.state.items.map((pantry_item, index) => {
+            <React.Fragment>
+                { this.state.items.map((pantry_item, index) => {
+                    if (storage_bin.includes(pantry_item.storage_bin)){
+                        
+                    } else {
+                    storage_bin.push(pantry_item.storage_bin)
+                    single_bin = pantry_item.storage_bin
                     return(
-                        <Col xs="3" sm="12" md={{ size: 2 }} style={{padding: '2px'}}>
-                        <Container  >
-                            <Card body key={ index }>
-                                <CardTitle>{ pantry_item.name }</CardTitle>
-                                <CardText>{ pantry_item.quantity } { pantry_item.units }</CardText>
+                        <Col style={{padding: '10px'}}>
+                            <Card body>
+                                <CardTitle>{pantry_item.storage_bin}</CardTitle>
+                                <Row>
+                                    { this.state.items.map((pantry_item, index) => {
+                                        if (pantry_item.storage_bin === single_bin) {
+                                            return(
+                                                <Col xs="3" sm="12" md={{ size: 2 }} style={{padding: '2px'}}>
+                                                    <Container  >
+                                                        <Card body key={ index }>
+                                                            <CardTitle>{ pantry_item.name }</CardTitle>
+                                                            <CardText>{ pantry_item.quantity } { pantry_item.units }</CardText>
+                                                        </Card>
+                                                    </Container>
+                                                </Col>
+                                                    )
+                                                }
+                                            }
+                                        )
+                                    }
+                                </Row>
                             </Card>
-                        </Container>
                         </Col>
-                                    )
-                                }
-                            )
-                         }
-                    </Row>
-                </Card>
-            </Col>
+                                )
+                            }
+                        }
+                    )
+                }
+            </React.Fragment>
         )
     }
 }
