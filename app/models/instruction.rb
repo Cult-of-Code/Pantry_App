906364d5b9c6f5b226703038b583a1e74f592e00
Ubuntu <ubuntu@ip-172-31-31-83.us-west-2.compute.internal>
2020-04-10 23:14:00 +0000

    added validations

--- app/models/instruction.rb
+++ app/models/instruction.rb
@@ -1,3 +1,4 @@
 class Instruction < ApplicationRecord
     belongs_to :user_recipes
+    validates :step, presence: true
 end
--- app/models/pantry_item.rb
+++ app/models/pantry_item.rb
@@ -1,3 +1,4 @@
 class PantryItem < ApplicationRecord
     belongs_to :user
+    validates :name, :quantity, :units, presence: true
 end
