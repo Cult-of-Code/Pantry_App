a778741b127fa5a81b572f5e991b9ece74a764b8
AustinMajeski <austinmajeski@gmail.com>
2020-04-14 00:21:10 +0000

    pantry api formatting and getting

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,13 @@ Rails.application.routes.draw do
   devise_for :users
   
   
+  
   # Custom Routes
   get '/user_packages' => 'main#user_packages_all'
   get '/user_package/:id' => 'main#user_package_one'
+  
+  get '/user_pantry_items/:id' => 'main#user_pantry_items'
+  
   
   
   get '*path', to: 'main#home', constraints: ->(request){ request.format.html? }
