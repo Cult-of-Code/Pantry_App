2f7a0d5c5afeef3c74e292b2fd892954901b75b1
AustinMajeski <austinmajeski@gmail.com>
2020-04-14 22:38:25 +0000

    added the ability to 'searchByIngredients' in the helper 'TheMealDB'  -  and need to finish 'getByID' and narrowing the detailed list

--- app/javascript/components/components/AustinsTest.js
+++ app/javascript/components/components/AustinsTest.js
@@ -2,8 +2,25 @@ import React, { Component } from 'react'
 //import { getRecipePuppy, getTheMealDB, addPantryItemToUser, getItemsFromUserPantry } from '../logical/fetchers'
 
 
+import TheMealDB from '../helpers/TheMealDB'
+
 export default class AustinsTest extends Component {
+    
+    constructor(){
+        super()
+        this.state= {}
+    }
+    
+    componentDidMount(){
+
+        TheMealDB.searchByIngredients(["Chicken","Basil"])
+        .then( receivedList => {
+          this.setState({ searchResults: receivedList }) 
+        })
+    }
+    
     render(){
+        console.table(this.state.searchResults)
         return(
             <h1>Austin</h1>
         )
