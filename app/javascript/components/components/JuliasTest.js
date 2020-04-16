a800a2ef5cb7e98be3460cd4e8a034a328308302
Ubuntu <ubuntu@ip-172-31-31-83.us-west-2.compute.internal>
2020-04-16 23:20:38 +0000

    added more to viewUserRecipes

--- app/javascript/components/components/JuliasTest.js
+++ app/javascript/components/components/JuliasTest.js
@@ -5,74 +5,22 @@ import { Navbar, NavbarBrand, Nav, NavItem, NavLink, NavbarText, Toast, ToastBod
 class JuliasTest extends Component {
   
 
-    render(){
-        return(
-            <React.Fragment>
-            <Navbar color="light" light expand="md">
-            <NavbarBrand href="/">Pantry App</NavbarBrand>
-            <Nav className="ml-auto" navbar>
-                    <NavItem>
-                      <NavLink href="">Get Recipes</NavLink>
-                    </NavItem>
-            {this.props.logged_in &&
-                    <NavItem>
-                      <NavLink href="">My Recipes</NavLink>
-                    </NavItem>
-            }
-            {!this.props.logged_in &&
-                    <NavItem>
-                      <NavLink href="/users/sign_up">Create an Account</NavLink>
-                    </NavItem>
-            }
-            </Nav>
-            </Navbar>
-            <div className="p-3 my-2 rounded bg-docs-transparent-grid">
-            {this.props.logged_in &&
-                <Toast>
-                  <ToastHeader>
-                    User Name 
-                  </ToastHeader>
-                  <ToastBody>
-                    Welcome (insert user name)! Lets get cooking!
-                    <Nav vertical>
-                        <NavItem>
-                          <NavLink href="#">Add Item to Pantry</NavLink>
-                        </NavItem>
-                        <NavbarText>
-                         About to Expire
-                        </NavbarText>
-                        <NavbarText>
-                         Running Low On
-                        </NavbarText>
-                    </Nav>
-                  </ToastBody>
-                </Toast>
-            }
-            {!this.props.logged_in &&
-                <Toast>
-                  <ToastHeader>
-                     Create an account to:
-                  </ToastHeader>
-                  <ToastBody>
-                    <Nav vertical>
-                        <NavbarText>
-                         Get recipies based off what you have in your pantry
-                        </NavbarText>
-                        <NavbarText>
-                         Track items about to Expire
-                        </NavbarText>
-                        <NavbarText>
-                         See groceries you are running Low On
-                        </NavbarText>
-                    </Nav>
-                  </ToastBody>
-                </Toast>
-            }
-            </div>
-            <div className="PageSlot">
-                {this.props.components}
-            </div>
-            </React.Fragment>
+   render(){
+      return (
+        <React.Fragment>
+            { this.props.user_recipes.map((user_recipe, index) => {
+                return(
+                    <Card body key={ index} >
+                        <CardTitle>{ user_recipe.name }</CardTitle>
+                        <CardText>
+                        { user_recipe.est_time } - { user_recipe.description }
+                        </CardText>
+                        <Button color="secondary" size="lg" block>See Recipe</Button>
+                    </Card>
+                )
+            })}
+       
+        </React.Fragment>
         )
     }
 
