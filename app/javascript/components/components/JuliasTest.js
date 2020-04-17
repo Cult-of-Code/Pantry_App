c38e3dee426cf2e824ab657c2169e94fe996e001
Ubuntu <ubuntu@ip-172-31-31-83.us-west-2.compute.internal>
2020-04-17 18:46:08 +0000

    fixed error on julia test page

--- app/javascript/components/components/JuliasTest.js
+++ app/javascript/components/components/JuliasTest.js
@@ -7,28 +7,10 @@ class JuliasTest extends Component {
   
 
    render(){
-        if (this.props.user_recipes !== null){
+        
       return (
-        <React.Fragment>
-            { this.props.user_recipes.map((user_recipe, index) => {
-                return(
-                    <Card body key={ index} >
-                        <CardTitle>{ user_recipe.name }</CardTitle>
-                        <CardText>
-                        { user_recipe.est_time } - { user_recipe.description }
-                        </CardText>
-                        <Button color="secondary" size="lg" block>See Recipe</Button>
-                    </Card>
-                )
-            })}
-       
-        </React.Fragment>
-        )
-        } else {
-            return (
-                <React.Fragment>
-                <h1> You do not have any recipes yet!</h1>
-                </React.Fragment>
+          <React.Fragment>
+          </React.Fragment>
         )
     }
 
