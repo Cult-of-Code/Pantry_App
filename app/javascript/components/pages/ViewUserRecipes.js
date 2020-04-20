cf7e5aea87e2fa271764f34bf3ed51726f21232a
Ubuntu <ubuntu@ip-172-31-31-83.us-west-2.compute.internal>
2020-04-20 19:56:00 +0000

    view user recipe index

--- app/javascript/components/pages/ViewUserRecipes.js
+++ app/javascript/components/pages/ViewUserRecipes.js
@@ -7,32 +7,28 @@ import { Card, CardTitle, CardText, Button, Row, Col } from 'reactstrap';
 class ViewUserRecipes extends Component {
     
     render(){
-    //     if (this.props.user_recipes !== null){
-    //   return (
-    //     <React.Fragment>
-    //         { this.props.user_recipes.map((user_recipe, index) => {
-    //             return(
-    //                 <Card body key={ index} >
-    //                     <CardTitle>{ user_recipe.name }</CardTitle>
-    //                     <CardText>
-    //                     { user_recipe.est_time } - { user_recipe.description }
-    //                     </CardText>
-    //                     <Button color="secondary" size="lg" block>See Recipe</Button>
-    //                 </Card>
-    //             )
-    //         })}
-       
-    //     </React.Fragment>
-    //     )
-    //     } else {
-            return (
+         return (
                 <React.Fragment>
                 <Row>
-               <Col> <h1> My Recipes</h1></Col>
-                 <Col><Button href= "/user/post/create" >Add New Recipe</Button></Col>
+                    <Col> <h1> My Recipes</h1></Col>
+                    <Col><Button href= "/user/post/create" >Add New Recipe</Button></Col>
                  </Row>
+      
+                { this.props.user_recipes.map((user_recipe, index) => {
+                    return(
+                        <Card body key={ index} >
+                            <CardTitle>{ user_recipe.name }</CardTitle>
+                            <CardText>
+                            { user_recipe.est_time } - { user_recipe.description }
+                            </CardText>
+                            <Button href= "/user/post/:post_id" color="secondary" size="lg" block>See Recipe</Button>
+                        </Card>
+                    )
+                })}
+       
                 </React.Fragment>
-                )
+        )
+           
         
     }
 }
